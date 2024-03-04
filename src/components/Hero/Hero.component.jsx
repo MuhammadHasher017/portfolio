4f9818f91d7e06867e62e8e873378f2af837c27d
hash017 <muhammad.hasher@redbuffer.net>
2024-03-04 22:21:23 +0500

    addition

--- src/components/Hero/Hero.component.jsx
+++ src/components/Hero/Hero.component.jsx
@@ -1,10 +1,31 @@
 import { motion } from "framer-motion";
+import { styles } from "../../styles";
 
-
-
-
+import { ComputersCanvas } from "../canvas";
 const Hero = () => {
-  return <section className="relative w-full h-screen mx-auto"></section>;
+  return (
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 flex flex-row items-start max-w-7xl top-[120px] gap-5  mx-auto`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className=" w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className=" text-[#915eff]">Hasher</span>{" "}
+          </h1>
+          <p className={` ${styles.heroSubText} mt-2 text-white-100`}>
+            I develop Full stack web Applications{" "}
+            <br className="sm:block  hidden" /> using nodejs and react.
+          </p>
+        </div>
+      </div>
+
+      <ComputersCanvas />
+    </section>
+  );
 };
 
 export default Hero;
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import React, { Suspense, useEffect, useState } from "react";
+import CanvasLoader from "../Loader/Loader.component";
 
 const Computers = () => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
-    <div>Computers</div>
-  )
-}
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor={"black"} />
+      <pointLight intensity={1} />
+      <primitive object={computer.scene} />
+    </mesh>
+  );
+};
 
-export default Computers
+export default Computers;
